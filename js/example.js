7a60e2f7a08e0e5567b80950e66c93dab0e3fd0e
MochiBao <113111181+MochiBao@users.noreply.github.com>
2023-01-19 17:26:00 +0200

    Add files via upload

---
+++ js/example.js
@@ -0,0 +1,14 @@
+const fruits = ["apples", "cherries", "oranges", "grapes", "lemons"];
+fruits.pop();
+fruits.push("watermelon")
+fruits.shift();
+fruits.unshift("garnet")
+
+console.log('fruits :>> ', fruits);
+
+fruits.splice(3, 1, "grapefruit", "kiwi", "peach");
+
+const message = ["A", "love", "JS"];
+
+console.log('message :>> ', message.join(" "));
+
